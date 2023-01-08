52a51ebb78589a658d5625954c2077e762008979
naz664 <nazimnavas145@gmail.com>
2023-01-08 19:34:35 +0530

    feat: Sync with latest sources of TrebleDroid

--- Arrow.mk
+++ Arrow.mk
@@ -10,5 +10,28 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
 ARROW_GAPPS := true
 
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+
+#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/ArrowOS_gsi/13/ota.json
+
+# Use CCache
+USE_CCACHE := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+        persist.sys.binary_xml=false \
+        dalvik.vm.dex2oat64.enabled=true \
+
+#prebuilts
+PRODUCT_PACKAGES += \
+  OpenEUICC \
+
+# Set Bootanimation at 720P
+TARGET_BOOT_ANIMATION_RES := 720
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
