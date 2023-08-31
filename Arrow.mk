9e6c24ea5cad62911dbe498f38a83c1942e116e7
naz664 <nazimnavas145@gmail.com>
2023-08-31 14:26:08 +0000

    feat: Sync with August source of Arrow
    Signed-off-by: naz664 <nazimnavas145@gmail.com>

--- Arrow.mk
+++ Arrow.mk
@@ -7,17 +7,10 @@ TARGET_NO_KERNEL_OVERRIDE := true
 TARGET_NO_KERNEL_IMAGE := true
 SELINUX_IGNORE_NEVERALLOWS := true
 
-TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
-TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
-
 ARROW_GAPPS := true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/ArrowOS_gsi/13.1/ota.json
-
-#prebuilts
-PRODUCT_PACKAGES += \
-  OpenEUICC \
 
 # Set Bootanimation at 720P
 TARGET_BOOT_ANIMATION_RES := 720
